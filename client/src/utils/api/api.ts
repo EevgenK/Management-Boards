5329c8cce4327b9bf56e15ac337536537716f224
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-05-27 20:17:03 +0300

    changed api url

--- client/src/utils/api/api.ts
+++ client/src/utils/api/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:4000' : '/api',
+  baseURL: import.meta.env.DEV
+    ? 'http://localhost:4000'
+    : 'https://management-boards.onrender.com/',
 });
